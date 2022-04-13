c65c591bf066ebd271b995cfea588544216b2c84
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-13 17:35:56 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dakota Riley on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
